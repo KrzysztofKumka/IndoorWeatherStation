aa4d5f1b708c357dc39dc61ccc2030a4cdc20700
Krzysztof Kumka <73432969+KrzysztofKumka@users.noreply.github.com>
2023-02-17 15:53:52 +0100

    Initial commit

---
+++ Core/Inc/fonts/fonts.h
@@ -0,0 +1,18 @@
+/*
+ * fonts.h
+ *
+ *  Created on: Feb 17, 2023
+ *      Author: krzysztof
+ */
+
+#ifndef INC_FONTS_FONTS_H_
+#define INC_FONTS_FONTS_H_
+
+#define FONT_8x5 1
+
+#if(FONT_8x5 == 1)
+#include "font_8x5.h"
+#endif
+
+
+#endif /* INC_FONTS_FONTS_H_ */
